d399e7f0630eee8146c1595c87baef0ec3415c24
Virtual Dick Master <eloi@jams.center>
2018-10-10 20:40:26 +0200

    Add of teams

---
+++ Assets/TeamLunarProject/Script/LoadMemberScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMemberScene : MonoBehaviour {
+
+    public string m_sceneName;
+	void Awake () {
+        SceneManager.LoadScene(m_sceneName,LoadSceneMode.Additive);
+	}
+	
+}
---
+++ Assets/TeamLunarProject/Script/OnlyOneCameraMain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OnlyOneCameraMain : MonoBehaviour {
+
+	void Awake () {
+        if (Camera.allCameras.Length>0 && Camera.main != this.GetComponent<Camera>()) {
+            Destroy(this.gameObject);
+        }
+	}
+	
+}
